6fe10b48fda8b3e85b8f70cbbcdaac4cc9a54243
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-08-04 19:25:25 +0530

    refactor: improve deleteArticle function by adding comment deletion logic

--- src/actions/delete-article.ts
+++ src/actions/delete-article.ts
@@ -2,13 +2,21 @@
 
 import { prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
- 
-export const deleteArticle = async (articleId: string) => {
 
+export const deleteArticle = async (articleId: string) => {
+    // Delete all comments associated with the article
+    await prisma.comment.deleteMany({
+        where: {
+            articleId: articleId,
+        },
+    });
+
+    // Delete the article
     await prisma.article.delete({
         where: {
             id: articleId,
         },
     });
+
     revalidatePath("/dashboard");
-}
+};
