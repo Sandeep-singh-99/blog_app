173146d539b2dbc82b1f38480282ab738369443d
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-08-18 22:55:09 +0530

    feat: add Bookmark model and related migration for article bookmarking functionality

---
+++ prisma/migrations/20250818172354_your_migration_name/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Bookmark" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "articleId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Bookmark_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/components/articles/bookmark-button.tsx
+++ src/components/articles/bookmark-button.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import { Bookmark } from 'lucide-react'
+import React from "react";
+import { Button } from "../ui/button";
+import { Bookmark } from "lucide-react";
 
 export default function BookmarkButton() {
   return (
     <div>
-         <Button className='gap-2' variant={'ghost'}>
-                <Bookmark className='h-5 w-5'/>
-            </Button>
+      <Button className="gap-2" variant={"ghost"}>
+        <Bookmark className="h-5 w-5" />
+      </Button>
     </div>
-  )
+  );
 }
