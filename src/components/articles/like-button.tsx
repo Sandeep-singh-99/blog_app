e06fd75e59eab8e32ff01e69fbb713334bfbe898
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-11-01 17:09:56 +0530

    feat: enhance LikeButton component with error handling and formatting improvements

--- src/components/articles/like-button.tsx
+++ src/components/articles/like-button.tsx
@@ -1,34 +1,46 @@
-'use client'
-import React, { useOptimistic, useTransition } from 'react'
-import { Button } from '../ui/button'
-import { ThumbsUp } from 'lucide-react'
-import { toggleLike } from '@/actions/like-toggle'
-import { Like } from '@prisma/client'
+"use client";
+import React, { useOptimistic, useTransition } from "react";
+import { Button } from "../ui/button";
+import { ThumbsUp } from "lucide-react";
+import { toggleLike } from "@/actions/like-toggle";
+import { Like } from "@prisma/client";
+import { toast } from "sonner";
 
 type LikeButtonProps = {
-    articleId: string,
-    likes: Like[],
-    isLiked: boolean
-}
+  articleId: string;
+  likes: Like[];
+  isLiked: boolean;
+};
 
-export default function LikeButton({ articleId, likes, isLiked }: LikeButtonProps) {
-    const [optimisticLike, setOptimisticLike] = useOptimistic(likes.length)
-    const [isPending, startTransition] = useTransition()
+export default function LikeButton({
+  articleId,
+  likes,
+  isLiked,
+}: LikeButtonProps) {
+  const [optimisticLike, setOptimisticLike] = useOptimistic(likes.length);
+  const [isPending, startTransition] = useTransition();
 
-    const handleLikeDisLike = async () => {
-        startTransition(async () => {
-            setOptimisticLike(isLiked ? optimisticLike - 1 : optimisticLike + 1)
-            await toggleLike(articleId)
-        })
-    }
+  const handleLikeDisLike = async () => {
+    startTransition(async () => {
+      try {
+        setOptimisticLike(isLiked ? optimisticLike - 1 : optimisticLike + 1);
+        await toggleLike(articleId);
+      } catch (error) {
+        toast.error(
+          error instanceof Error ? error.message : "Error liking the article"
+        );
+      }
+    });
+  };
+
   return (
-    <div className=''>
-        <form action={handleLikeDisLike}>
-            <Button disabled={isPending} className='gap-2' variant={'ghost'}>
-                <ThumbsUp className='h-5 w-5'/>
-                {optimisticLike}
-            </Button>
-        </form>
+    <div className="">
+      <form action={handleLikeDisLike}>
+        <Button disabled={isPending} className="gap-2" variant={"ghost"}>
+          <ThumbsUp className="h-5 w-5" />
+          {optimisticLike}
+        </Button>
+      </form>
     </div>
-  )
+  );
 }
