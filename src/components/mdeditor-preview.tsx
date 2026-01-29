0e38afe7a91dc6ffcd37a708345095e311738b2d
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-01-29 21:29:27 +0530

    feat: Initialize global styles with Tailwind, theming, and add Markdown editor preview component.

--- src/components/mdeditor-preview.tsx
+++ src/components/mdeditor-preview.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React from 'react'
 import MDEditor from '@uiw/react-md-editor';
+import { useTheme } from 'next-themes';
 
 export default function MdEditorPreview({ content }: { content: string }) {
+  const { theme } = useTheme();
   return (
-    <MDEditor.Markdown source={content} className='p-4 rounded-md w-full' />
+    <div data-color-mode={theme}>
+      <MDEditor.Markdown source={content} className='p-4 rounded-md w-full' style={{ backgroundColor: "transparent" }} />
+    </div>
   )
 }
